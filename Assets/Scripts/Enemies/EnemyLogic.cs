b2333eb48c3ca42c4e7170376066c77842b7b079
SimonOsorio <isabelahdez03@gmail.com>
2022-05-01 21:44:00 -0500

    Logica Player y Enemigos

---
+++ Assets/Scripts/Enemies/EnemyLogic.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLogic : MonoBehaviour
+{
+    [SerializeField] int health = 3;
+    public InGameObject enemyStats;
+    
+
+    private void Start()
+    {
+        enemyStats = new InGameObject();
+        enemyStats.SetHealth(health);
+        enemyStats.HealthText = GetComponentInChildren<TMPro.TextMeshPro>();
+    }
+
+    private void Update()
+    {
+        health = enemyStats.GetHealth();
+        if (health <= 0) { Destroy(transform.parent.gameObject); }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerLogic.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    [SerializeField] int initialHealth = 5;
+    [SerializeField] int currentHealth;
+    public InGameObject playerStats;
+
+    private void Start()
+    {
+        playerStats = new InGameObject(); playerStats.SetHealth(initialHealth);
+        playerStats.HealthText = GetComponentInChildren<TMPro.TextMeshPro>();
+    }
+
+    private void Update()
+    {
+        currentHealth = playerStats.GetHealth();
+        if (currentHealth <= 0)
+        {
+            Debug.Log("Game Over");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            EnemyLogic enemyLogic = collision.GetComponent<EnemyLogic>();
+            int enemyHealth = enemyLogic.enemyStats.GetHealth();
+
+            if (enemyHealth < currentHealth)
+            {
+                enemyLogic.enemyStats.RecieveDamage(playerStats.DealDamage(enemyHealth));
+            }
+            else
+            {
+                playerStats.RecieveDamage(enemyLogic.enemyStats.DealDamage(currentHealth));
+            }
+
+        }
+    }
+
+}
